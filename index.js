d74c8c26b8ae442161aa369d6ee61593e589daac
jothishwaran <113631008+Vignesh2913@users.noreply.github.com>
2024-02-12 13:24:23 +0530

    Update

--- index.js
+++ index.js
@@ -11,9 +11,14 @@ app.set('port', process.env.PORT || 5000);
 
 // Home page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'home.html'));
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
+
+app.get('/chat', (req, res) => {
+    res.sendFile(path.join(__dirname, "public", 'Chat.html'));
+})
+
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+    console.log("Node app is running at localhost:" + app.get('port'))
 })
